655262e94f081cd1281ebb2bb6ba040c5d4e8809
DillEmma <emmabauta@gmail.com>
2021-03-03 13:10:17 -0500

    piechart

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 //import './App.css';
 import Wrapper from "./components/Wrapper";
-import Homepage from './Pages/Homepage';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <Router>
       <div>
         <Wrapper>
-          <Route exact path="/" component={Homepage} />
+          <Route exact path="/" component={Home} />
         </Wrapper>
       </div>
     </Router>
