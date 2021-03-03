4eddf7b3e0ec1f046fc6b09bec8f93ca684ad355
Anthony Loredo <anthonyloredo5@gmail.com>
2021-03-03 15:40:45 -0500

    saved conflict change

--- client/src/Pages/Landing.js
+++ client/src/Pages/Landing.js
@@ -3,11 +3,7 @@ import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
-<<<<<<< HEAD
-import background from "../images/background1.png"
-=======
 import background from "../images/background1.png";
->>>>>>> 27171226b10f9b36431c6f6e5f230902b498828b
 import ModalSignUp from "../components/modalSignUp"; 
 
 
