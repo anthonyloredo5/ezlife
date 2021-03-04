3b0e65acca4f74caee2cfe8907816864cf3d8143
Remmington Pascone <remmingtonpearce@gmail.com>
2021-03-04 10:22:38 -0800

    added quotes to jumbo that changes with heart button

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Quote from "./Quote"; 
 
 function Jumbotron() {
     return (
     < div class="jumbotron jumbotron-fluid" >
         <div class="container">
             <h1 class="text-center">ezlife</h1>
-            <p class="lead text-center">This will be where the inspirational quote will be. </p>
+            <br></br>
+            <br></br>
+            <Quote />
             </div>
     </div >
     )
