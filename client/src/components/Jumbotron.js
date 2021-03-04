1a7cb33b1cbc35a00cb44523252eccf3cb9f7285
Bethany Green <b.gree10@gail.com>
2021-03-04 12:22:31 -0600

    rendered image onto jumbotron

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -1,12 +1,28 @@
 import React from "react";
+import Hero from "../components/Hero";
+import background from "../images/jumbo.png"
 
 function Jumbotron() {
     return (
-    < div class="jumbotron jumbotron-fluid" >
-        <div class="container">
+    <div class="jumbotron jumbotron-fluid container justify ='center' "  style={{
+    width: "1000px",
+    paddingTop: "0px",
+    paddingBottom: "0px",
+    marginBottom: "20px",
+    height: "500px",
+    opacity: "0.5",
+    
+      }} >
+        <Hero  backgroundImage= {background}  > 
+        <div style={{
+            
+        }}>
             <h1 class="text-center">ezlife</h1>
-            <p class="lead text-center">This will be where the inspirational quote will be. </p>
+            <p class="lead text-center">affirmations will render here </p>
             </div>
+        </Hero>
+    
+        
     </div >
     )
 }
