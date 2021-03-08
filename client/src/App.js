e30b48c345e27c5f32c4b7899d313bbc7ca32ffb
DillEmma <emmabauta@gmail.com>
2021-03-08 11:37:04 -0500

    added calendar for workouts

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import Landing from './Pages/Landing';
 import Dash from "./Pages/Dash";
-
+import Workouts from "./Pages/Workouts";
 import ThemeContext from './Context.js'
 import CreateAccount from './components/CreateAccount';
 import Login from './components/Login';
@@ -39,7 +39,7 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route exact path="/dash" component={Dash} />
 
-
+          <Route exact path ="/fitness" component={Workouts} />
           <Route exact path="/signUp" component={CreateAccount} />
           <Route exact path="/Login" component={Login} />
           <Route path="/timer" component={Timer} />
