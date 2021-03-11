a7a70ac02dff80ac3749715197c53578a8ecc0f5
Remmington Pascone <remmingtonpearce@gmail.com>
2021-03-10 17:37:18 -0800

    added the navbar with functional links to every page

--- client/src/Pages/Budget.js
+++ client/src/Pages/Budget.js
@@ -1,8 +1,10 @@
 import React from "react"; 
+import Drawer from '../components/Drawer';
 
 function Budget () { 
     return ( 
         <>
+        <Drawer  />
         <h1> This page allows the user to input credits and debits to their finances to see a better idea of their habits.</h1>
         </>
     )
